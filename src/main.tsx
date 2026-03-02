69a1cc06845e9a94af1a50eb7d8c7dbce2c13c20
2shining <fetrafaneva@gmail.com>
2026-03-02 20:59:57 +0300

    feat: update main.tsx for the windows action

--- src/main.tsx
+++ src/main.tsx
@@ -55,3 +55,14 @@ function createWindow(): void {
     };
   });
 }
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
